20814bef694800029e22f13ae82e7692c5eb0fcd
nisarg <nisargpatel947@gmail.com>
2026-05-09 16:26:06 +0530

    done

--- src/main/java/com/practice/security/JwtAuthenticationFilter.java
+++ src/main/java/com/practice/security/JwtAuthenticationFilter.java
@@ -1,71 +1,17 @@
 package com.practice.security;
-
-//package com.practice.security;
-
-//
-//import java.io.IOException;
-//
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//
-//@Component
-//
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//
-//	private final JwtService jwtService;
-//	private final CustomUserDetailsService userDetailsService;
-//
-//	public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService userDetailsService) {
-//		this.jwtService = jwtService;
-//		this.userDetailsService = userDetailsService;
-//	}
-//  
-//	@Override
-//	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-//			throws ServletException, IOException {
-//
-//		String header = request.getHeader("Authorization");
-//
-//		if (header != null && header.startsWith("Bearer ")) {
-//			String token = header.substring(7);
-//			String email = jwtService.extractUsername(token);
-//
-//			// if valid user(email) is there and user is not authenticated yet then extract user from DB
-//			if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//				UserDetails user = userDetailsService.loadUserByUsername(email);
-//
-//				if (jwtService.validateToken(token, user.getUsername())) {
-//					SecurityContextHolder.getContext().setAuthentication(
-//							new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
-//				}
-//			} 
-//		}
-//
-//		chain.doFilter(request, response);
-//	}
-//
-//}
-
 import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
- 
+ @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final JwtService jwtService;
 	private final CustomUserDetailService userDetailService;
